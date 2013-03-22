5bf468d1ba37df39b0787d8050082982355e954f
Gavin M. Roy <gmr@meetme.com>
2013-03-22 14:50:24 -0400

    Bump the rev

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('MANIFEST.in', 'w') as handle:
             handle.write('include %s\n' % filename)
 
 setup(name='statelessd',
-      version='0.0.3',
+      version='0.0.4',
       description='Stateless HTTP -> AMQP gateway',
       url='http://github.com/gmr/statelessd',
       packages=['statelessd'],
--- statelessd/__init__.py
+++ statelessd/__init__.py
@@ -2,7 +2,7 @@
 Stateless HTTP -> AMQP publishing
 
 """
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 from statelessd.dashboard import Dashboard
 from statelessd.publisher import Publisher
